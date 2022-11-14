3aafd928f79dc230337944ba703cda3790ed38ad
Angelika Serwa <angelika.serwa@swmansion.com>
2022-11-14 16:39:38 +0100

    [MV-219] Add `peerRemoved` and `encodingChanged` events handling (#18)
    * [MV-219] Add `peerRemoved` and `encodingChanged` events handling
    
    * Add default implementations

--- MembraneRTC/src/main/java/org/membraneframework/rtc/MembraneRTCListener.kt
+++ MembraneRTC/src/main/java/org/membraneframework/rtc/MembraneRTCListener.kt
@@ -2,6 +2,7 @@ package org.membraneframework.rtc
 
 import org.membraneframework.rtc.models.Peer
 import org.membraneframework.rtc.models.TrackContext
+import timber.log.Timber
 
 public interface MembraneRTCListener {
     // / Callback invoked when client has successfully connected via transport layer.
@@ -34,6 +35,16 @@ public interface MembraneRTCListener {
     // /Callback invoked when peer's metadata gets updated.
     fun onPeerUpdated(peer: Peer)
 
+    // Callback invoked when received track encoding has changed
+    fun onTrackEncodingChanged(peerId: String, trackId: String, encoding: String) {
+        Timber.i(
+            "Track encoding changed $trackId -> $encoding"
+        )
+    }
+
     // /Callback invoked when an errors happens.
     fun onError(error: MembraneRTCError)
+
+    // Callback invoked every time a local peer is removed by the server
+    fun onRemoved(reason: String) { Timber.e("Peer removed") }
 }
