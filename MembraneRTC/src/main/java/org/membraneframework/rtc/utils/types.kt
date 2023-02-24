1bdc38cbc0f371626549525889cc6a5e856638f0
Angelika Serwa <angelika.serwa@swmansion.com>
2023-02-24 22:45:02 +0100

    Add socket channel params

--- MembraneRTC/src/main/java/org/membraneframework/rtc/utils/types.kt
+++ MembraneRTC/src/main/java/org/membraneframework/rtc/utils/types.kt
@@ -3,3 +3,4 @@ package org.membraneframework.rtc.utils
 typealias Payload = Map<String, Any?>
 typealias Metadata = Map<String, Any>
 typealias SocketConnectionParams = Map<String, Any>
+typealias SocketChannelParams = Map<String, Any>
