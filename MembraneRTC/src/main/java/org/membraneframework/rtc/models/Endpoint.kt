3a279c98be5525a85e0c365ecd0fb63894660f47
Mikolaj <mikolaj.kozakiewicz@swmansion.com>
2024-01-10 11:25:02 +0100

    Add default metadata to Endpoint and connect

--- MembraneRTC/src/main/java/org/membraneframework/rtc/models/Endpoint.kt
+++ MembraneRTC/src/main/java/org/membraneframework/rtc/models/Endpoint.kt
@@ -5,10 +5,13 @@ import org.membraneframework.rtc.utils.Metadata
 data class Endpoint(
     val id: String,
     val type: String,
-    val metadata: Metadata = mapOf(),
+    val metadata: Metadata? = mapOf(),
     val trackIdToMetadata: Map<String, Metadata> = mapOf()
 ) {
-    fun withTrack(trackId: String, metadata: Metadata?): Endpoint {
+    fun withTrack(
+        trackId: String,
+        metadata: Metadata?
+    ): Endpoint {
         val newTrackIdToMetadata = this.trackIdToMetadata.toMutableMap()
         newTrackIdToMetadata[trackId] = metadata ?: mapOf()
         return this.copy(trackIdToMetadata = newTrackIdToMetadata)
