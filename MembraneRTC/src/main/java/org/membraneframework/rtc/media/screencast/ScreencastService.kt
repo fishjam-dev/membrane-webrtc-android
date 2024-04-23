682bcdab1833f9c7c390d13769013f7e71da1f24
Karol Kąkol <kakol.karol00@gmail.com>
2024-04-23 20:02:29 +0200

    change onTaskRemoved to onDestory method function in ScreencastService v1

--- MembraneRTC/src/main/java/org/membraneframework/rtc/media/screencast/ScreencastService.kt
+++ MembraneRTC/src/main/java/org/membraneframework/rtc/media/screencast/ScreencastService.kt
@@ -9,14 +9,18 @@ import android.content.Intent
 import android.os.Binder
 import android.os.Build
 import android.os.IBinder
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
+
+const val TAG = "SCREENCAST"
 
 internal class ScreencastService : Service() {
     private var binder: IBinder = ScreencastBinder()
     private var bindCount = 0
 
     override fun onBind(intent: Intent?): IBinder {
+        Log.d(TAG, "onBind")
         bindCount++
         return binder
     }
@@ -25,6 +29,8 @@ internal class ScreencastService : Service() {
         notificationId: Int?,
         notification: Notification?
     ) {
+        Log.d(TAG, "start")
+
         val properNotification =
             if (notification != null) {
                 notification
@@ -42,12 +48,24 @@ internal class ScreencastService : Service() {
     }
 
     override fun onDestroy() {
+        Log.d(TAG, "onDestroy")
+
+        stopForeground(true)
+        stopSelf()
+    }
+
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        Log.d(TAG, "onTaskRemoved")
+
+        super.onTaskRemoved(rootIntent)
         stopForeground(true)
         stopSelf()
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel() {
+        Log.d(TAG, "createNotificationChannel")
+
         val channel =
             NotificationChannel(
                 DEFAULT_CHANNEL_ID,
@@ -61,6 +79,8 @@ internal class ScreencastService : Service() {
     }
 
     override fun onUnbind(intent: Intent?): Boolean {
+        Log.d(TAG, "onUnbind")
+
         bindCount--
 
         if (bindCount == 0) {
